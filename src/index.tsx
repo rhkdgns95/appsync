41064cf3633ab6fe66e3316ef44cf12a4cb4e19d
rhkdgns9489@gmail.com <rhkdgns9489@gmail.com>
2020-03-28 13:20:29 +0900

    #1.0 AWS Appsync Setup with Amplify

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync'
+import AppSyncConfig from './aws-exports'
+import { ApolloProvider } from 'react-apollo'
+import { Rehydrated } from 'aws-appsync-react' // this needs to also be installed when working with React
+
+const client = new AWSAppSyncClient({
+  url: AppSyncConfig.aws_appsync_graphqlEndpoint,
+  region: AppSyncConfig.aws_appsync_region, 
+  auth: {
+    type: AppSyncConfig.aws_appsync_authenticationType as AUTH_TYPE | any,
+    apiKey: AppSyncConfig.aws_appsync_apiKey,
+    // jwtToken: async () => token, // Required when you use Cognito UserPools OR OpenID Connect. token object is obtained previously
+  }
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
